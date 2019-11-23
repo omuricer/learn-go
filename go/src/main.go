41ba88a109561abef6c7075b66fcbe36bd101fbb
omuricer <guts.inaq@gmail.com>
2019-11-23 23:01:30 +0900

    DB select

--- go/src/main.go
+++ go/src/main.go
@@ -1,20 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
 type Letter struct {
-	ID			int		`json:id`
-	Title		string	`json:title`
-	Author		string	`json:author`
-	PostDate	string	`json:yaar`
+	ID       int    `json:id`
+	Title    string `json:title`
+	Author   string `json:author`
+	PostDate string `json:yaar`
 }
 
 var letters []Letter
@@ -35,11 +37,31 @@ func getLetter(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	i, _ := strconv.Atoi(params["id"])
 
-	for _, letter := range letters {
-		if letter.ID == i {
-			json.NewEncoder(w).Encode(letter)
-		}
+	db, err := sql.Open("mysql", "account:account@tcp(db:3306)/account_db")
+	if err != nil {
+		log.Fatal("db err!!")
 	}
+
+	type Result struct {
+		time int
+	}
+	rows, err := db.Query("select 123")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var results []Result
+	// fmt.Println(rows)
+	for rows.Next() {
+		result := Result{}
+		if err := rows.Scan(&result.time); err != nil {
+			log.Fatal(err)
+		}
+		results = append(results, result)
+	}
+
+	fmt.Println(i)
+	fmt.Println(results)
 }
 
 func addLetter(w http.ResponseWriter, r *http.Request) {
@@ -62,8 +84,8 @@ func removeArticle(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleRequests() {
-    router := mux.NewRouter().StrictSlash(true)
-    router.HandleFunc("/", homePage).Methods("GET")
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homePage).Methods("GET")
 	router.HandleFunc("/letter", getLetters).Methods("GET")
 	router.HandleFunc("/letter/{id}", getLetter).Methods("GET")
 	router.HandleFunc("/letter", addLetter).Methods("PUT")
@@ -71,13 +93,13 @@ func handleRequests() {
 }
 
 func main() {
-	letters = append(letters, 
-		Letter{ID: 1,	Title: "test1",		Author: "auth1",	PostDate: "2019/01/01"},
-		Letter{ID: 2,	Title: "test2",		Author: "auth2",	PostDate: "2019/01/02"},
-		Letter{ID: 3,	Title: "test3",		Author: "auth3",	PostDate: "2019/01/03"},
-		Letter{ID: 4,	Title: "test4",		Author: "auth4",	PostDate: "2019/01/04"},
+	letters = append(letters,
+		Letter{ID: 1, Title: "test1", Author: "auth1", PostDate: "2019/01/01"},
+		Letter{ID: 2, Title: "test2", Author: "auth2", PostDate: "2019/01/02"},
+		Letter{ID: 3, Title: "test3", Author: "auth3", PostDate: "2019/01/03"},
+		Letter{ID: 4, Title: "test4", Author: "auth4", PostDate: "2019/01/04"},
 	)
 
 	handleRequests()
-	
+
 }
